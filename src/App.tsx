61b0d1653a8e006c9fd091b37c22b31ad9c04bbc
GGGavin <z13237002887@gmail.com>
2025-10-08 13:39:17 +0100

    fix: Update routing for GitHub Pages base path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+
+
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+
 
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -12,6 +15,19 @@ import GalleryPage from './pages/GalleryPage';
 function App() {
   return (
     <Routes>
+      
+      <Route path="/FirstPortForlio/" element={
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/skills" element={<SkillsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+        </Routes>
+      } />
+     
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/projects" element={<ProjectsPage />} />
